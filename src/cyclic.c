e4642f1eac7baffc1d15ec2bb656c83f8f979482
nlohmill <nils.lohmiller@hs-esslingen.de>
2024-11-19 09:19:24 +0100

    add all cyclic groups between 2^32 and 2^48

--- src/cyclic.c
+++ src/cyclic.c
@@ -90,21 +90,71 @@ static cyclic_group_t groups[] = {
      .known_primroot = 3,
      .prime_factors = {2, 83, 1277, 20261},
      .num_prime_factors = 4},
+	 {// 2^35 + 13
+     .prime = 34359738381,
+     .known_primroot = 2, 
+     .prime_factors = {2,5,11,577,2237},
+     .num_prime_factors = 5},
     {// 2^36 + 31
      .prime = 68719476767,
      .known_primroot = 5,
      .prime_factors = {2, 163, 883, 238727},
      .num_prime_factors = 4},
+	 {// 2^37 + 51
+     .prime = 137438953523,
+     .known_primroot = 2,
+     .prime_factors = {2,13,809,6534133},
+     .num_prime_factors = 4},
+	 {// 2^38 + 3
+     .prime = 274877906947,
+     .known_primroot = 2,
+     .prime_factors = {2, 3, 1777, 25781083},
+     .num_prime_factors = 4},
+	 {// 2^39 + 23
+     .prime = 549755813911,
+     .known_primroot = 2,
+     .prime_factors = {2, 3, 5, 383, 47846459},
+     .num_prime_factors = 5},
     {// 2^40 + 15
      .prime = 1099511627791,
      .known_primroot = 3,
      .prime_factors = {2, 3, 5, 36650387593},
      .num_prime_factors = 4},
+	 {// 2^41 + 27
+     .prime = 2199023255579,
+     .known_primroot = 2,
+     .prime_factors = {2,277,3969356057},
+     .num_prime_factors = 3},
+	 {// 2^42 + 15
+     .prime = 4398046511119,
+     .known_primroot = 2,
+     .prime_factors = {2,3,13,71,227,3498493},
+     .num_prime_factors = 6},
+	 {// 2^43 + 29
+     .prime = 8796093022237,
+     .known_primroot = 2,
+     .prime_factors = {2,3,13,71,227,3498493},
+     .num_prime_factors = 6},
     {// 2^44 + 7
      .prime = 17592186044423,
      .known_primroot = 5,
      .prime_factors = {2, 11, 53, 97, 155542661},
      .num_prime_factors = 5},
+	 {// 2^45 + 59
+     .prime = 35184372088891,
+     .known_primroot = 5,
+     .prime_factors = {2, 3, 5, 19, 120739, 511243},
+     .num_prime_factors = 6},
+	 {// 2^46 + 15
+     .prime = 70368744177679,
+     .known_primroot = 2,
+     .prime_factors = {2,3,1947973,6020681},
+     .num_prime_factors = 4},
+	 {// 2^47 + 5
+     .prime = 140737488355333,
+     .known_primroot = 2,
+     .prime_factors = {2,3,11,19,331,18837001},
+     .num_prime_factors = 6},
     {// 2^48 + 23
      .prime = 281474976710677,
      .known_primroot = 6,
